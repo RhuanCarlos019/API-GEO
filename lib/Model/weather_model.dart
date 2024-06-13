fa391a8e11835fe19a17b72a9b9ca8f32f14e0c6
Rhuan Carlos <123770612+RhuanCarlos019@users.noreply.github.com>
2024-06-13 08:41:02 -0300

    Ok

--- lib/Model/weather_model.dart
+++ lib/Model/weather_model.dart
@@ -1,17 +1,23 @@
 class Weather {
-  //atributos
   final String name;
   final String description;
   final String main;
   final double temp;
   final double tempMax;
   final double tempMin;
+  final String icon; // Novo atributo para o ícone
 
-  //construtor
-  Weather({required this.name, required this.description, required this.main, required this.temp, required this.tempMax, required this.tempMin});
+  Weather({
+    required this.name,
+    required this.description,
+    required this.main,
+    required this.temp,
+    required this.tempMax,
+    required this.tempMin,
+    required this.icon, // Novo argumento para o construtor
+  });
 
-  //método
-  factory Weather.fromJson(Map<String,dynamic> json) {
+  factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
       name: json['name'],
       description: json['weather'][0]['description'],
@@ -19,6 +25,17 @@ class Weather {
       temp: json['main']['temp'],
       tempMax: json['main']['temp_max'],
       tempMin: json['main']['temp_min'],
+      icon: json['weather'][0]['icon'], // Ícone adicionado ao parsing
     );
+  }
+
+  String getNotificationMessage() {
+    if (temp < 10) {
+      return "Agasalhe-se, está frio";
+    } else if (temp > 30) {
+      return "Se hidrate, o calor está insano";
+    } else {
+      return "A temperatura está agradável";
+    }
   }
 }
