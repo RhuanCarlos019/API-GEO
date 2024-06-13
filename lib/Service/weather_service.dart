208694c9ee73b703872ac87a46f85a3c3d69f730
Rhuan Carlos <123770612+RhuanCarlos019@users.noreply.github.com>
2024-06-13 08:43:19 -0300

    Ok

--- lib/Service/weather_service.dart
+++ lib/Service/weather_service.dart
@@ -2,13 +2,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  //atributos
   final String apiKey = "9a180e5bbd3fbe3fd2b5cd74cb5bfc6f";
   final String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-  //metodos
   Future<Map<String, dynamic>> getWeather(String city) async {
-    final url = Uri.parse('$baseUrl?q=$city&appid=$apiKey');
+    final url = Uri.parse('$baseUrl?q=$city&appid=$apiKey&units=metric'); // Adicionado units=metric para temperaturas em Celsius
     final response = await http.get(url);
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
@@ -17,9 +15,8 @@ class WeatherService {
     }
   }
 
-  Future<Map<String, dynamic>> getWeatherbyLocation(
-      double lat, double lon) async {
-    final url = Uri.parse('$baseUrl?lat=$lat&lon=$lon&appid=$apiKey');
+  Future<Map<String, dynamic>> getWeatherbyLocation(double lat, double lon) async {
+    final url = Uri.parse('$baseUrl?lat=$lat&lon=$lon&appid=$apiKey&units=metric'); // Adicionado units=metric para temperaturas em Celsius
     final response = await http.get(url);
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
